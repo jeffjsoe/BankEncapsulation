192dbf4dcd9100560539683af5ecb0c958cfda64
Jeffrey Jose <jefjosecool@gmail.com>
2022-10-31 10:55:55 -0400

    "Finished"

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿//JESUS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        //We are using all these methods because we dont have sirect access to balance
+        private double balance=0;
+
+
+
+        public BankAccount(double amount)
+        {
+            this.balance = amount;
+
+
+        }
+
+        public void deposit(double amount)
+        {
+            balance = balance + amount;
+            Console.WriteLine("Deposit accepted");
+        }
+        public double getbalance()
+        {
+            return balance;
+
+        }
+
+
+    }
+    
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace BankEncapsulation
+﻿
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myaccount = new BankAccount(0);
+            Console.WriteLine("How much do you want to deposit");
+            double amounttodeposit = double.Parse(Console.ReadLine());
+            myaccount.deposit(amounttodeposit);
+
+            double userbalance = myaccount.getbalance();
+            Console.WriteLine($"You have {userbalance} as your balance");
         }
     }
 }
